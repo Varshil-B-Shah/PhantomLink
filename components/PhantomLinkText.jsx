ec63cb175507d47faeff62f6255d8261a8fc1639
Varshil <varshil4247shah@gmail.com>
2025-03-07 16:36:20 +0530

    yello

--- components/PhantomLinkText.jsx
+++ components/PhantomLinkText.jsx
@@ -9,7 +9,7 @@ const PhantomLinkText = () => {
         text-blue-300 font-phantom dark:text-blue-300 opacity-80 hover:opacity-100 hover:text-cyan-400 transition-all duration-300 tracking-wide shadow-md hover:shadow-cyan-500/50 cursor-pointer
       "
       >
-        <Link href="/metrics">Phantom Link</Link>
+        <Link href="/com">Phantom Link</Link>
       </p>
     </div>
   );
