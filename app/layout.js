6a2a47b9c677fe076fc14a53936aa36fbee3949b
Varshil <varshil4247shah@gmail.com>
2025-03-12 17:58:48 +0530

    i hate deployment

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey="pk_test_ZGVzaXJlZC1mb3dsLTkyLmNsZXJrLmFjY291bnRzLmRldiQ">
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
